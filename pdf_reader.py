eff1af3642aefe5529a64dac0c373a62efa144b4
MovingToWeb <soleil.fils.88@gmail.com>
2020-03-25 19:33:41 +0530

    @update use basics.py for learning basic syntaxes

--- pdf_reader.py
+++ pdf_reader.py
@@ -8,13 +8,13 @@ pdfFileObj = open('example.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
   
 # printing number of pages in pdf file 
-print(pdfReader.numPages) 
+print("No of pages: ", pdfReader.numPages) 
   
 # creating a page object 
 pageObj = pdfReader.getPage(0) 
   
 # extracting text from page 
-print(pageObj.extractText()) 
+print("Text: ", pageObj.extractText()) 
   
 # closing the pdf file object 
 pdfFileObj.close() 
