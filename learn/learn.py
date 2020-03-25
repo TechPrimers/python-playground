24a764fae3b79ec38f04c43d1ad6c35e3c51dc08
MovingToWeb <soleil.fils.88@gmail.com>
2020-03-25 20:27:37 +0530

    @update modules usage

---
+++ learn/learn.py
@@ -0,0 +1,6 @@
+class Learn:
+    def __init__(self, text):
+        self.text = text
+
+    def share(self):
+        return self.text
---
+++ modules.py
@@ -0,0 +1,4 @@
+import learn
+
+l = learn.Learn("This is a learning program")
+print(l.share())
