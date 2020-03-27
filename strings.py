beb62838c9990a283a667d13cbb02ab32c5328d6
MovingToWeb <soleil.fils.88@gmail.com>
2020-03-27 19:32:00 +0530

    @update use strings

---
+++ strings.py
@@ -0,0 +1,13 @@
+extractor="Hello World"
+print("Full String: ", extractor[:])
+
+print("Offset from 3: ", extractor[3:])
+print("Offset from :3 ", extractor[:3])
+print("Offset from 3:8 ", extractor[3:8])
+
+print("Last 3 characters: ", extractor[-3:])
+print("Last 3 characters: ", extractor[4:-3])
+
+print("By space: ", extractor.split(" "))
+
+
