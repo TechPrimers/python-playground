2467024e422862c8c7136d9864670b01977f25f2
MovingToWeb <soleil.fils.88@gmail.com>
2020-04-01 23:11:58 +0530

    @add day 8 updates

---
+++ classes/DataClass.py
@@ -0,0 +1,6 @@
+from dataclasses import dataclass
+
+@dataclass
+class DataClass:
+    name: str
+    age: int = 0
---
+++ classes/dataclass_main.py
@@ -0,0 +1,6 @@
+from DataClass import DataClass
+
+data_class = DataClass("ajay", 19)
+
+print("Name: ", data_class.name)
+print("Age: ", data_class.age)
