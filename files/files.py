1fd9b5e1d2a072d565775d067153e22559694b05
MovingToWeb <soleil.fils.88@gmail.com>
2020-04-03 22:16:15 +0530

    @update: day 10

--- files/files.py
+++ files/files.py
@@ -1,5 +1,5 @@
 file_obj = open("files.txt", "wt")
-print("This is a temp file ", file=file_obj)
+print("This is a temp file", file=file_obj)
 file_obj.write("Second line")
 file_obj.close()
 
@@ -8,3 +8,18 @@ try:
     fout.write('added')
 except FileExistsError:
     print('files.txt already exists!')
+
+file_obj = open('files.txt', 'rt')
+while True:
+    line = file_obj.readline();
+    if not line:
+        break;
+    print("Line: ", line)
+file_obj.close()
+
+file_obj = open('files.txt', 'rt')
+lines = file_obj.readlines();
+print("Lines: ", lines)
+file_obj.close()
+for line in lines:
+    print("Line...", line)
